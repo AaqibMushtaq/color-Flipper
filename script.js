c6f2e3e3cf12475bc165307d1c619069d32fde48
Aaqib Mushtaq <dev.aaqibmushtaq@gmail.com>
2024-01-10 10:16:09 +0530

    add styles

---
+++ script.js
@@ -0,0 +1,13 @@
+const colors = ["red", "grey", "#8e6fc1d6", "rgb(91, 206, 139)"];
+const button = document.getElementById("button");
+const randomColor = document.querySelector(".random_color");
+
+button.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  randomColor.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
